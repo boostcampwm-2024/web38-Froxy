37062ab792603abb985332f2f4c7f60ccf899cb0
mjh000526 <mjh000526@gmail.com>
2024-11-07 15:01:53 +0900

    feat: #9 backend env

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DockerModule } from './docker/docker.module';
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [DockerModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DockerModule],
   controllers: [AppController],
   providers: [AppService]
 })
--- apps/backend/src/docker/docker.controller.ts
+++ apps/backend/src/docker/docker.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { DockerService } from './docker.service.js';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('docker')
 export class DockerController {
-  constructor(private readonly dockerService: DockerService) {}
+  constructor(private readonly dockerService: DockerService, private configService: ConfigService) {}
 
   @Get('get')
   async getDockersTest(): Promise<string>{
     const mainFileName = 'OriginalCode.js';
-    const gitToken = "";
+    const gitToken = this.configService.get<string>('GIT_TOKEN');
+    console.log(gitToken);
     return await this.dockerService.getDocker(gitToken, mainFileName);
   }
 }
