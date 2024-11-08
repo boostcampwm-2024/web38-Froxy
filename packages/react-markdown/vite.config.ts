f0c178f9abf28e1f4f7b25dc210f2cce54e5efd5
ATeals <ateals@icloud.com>
2024-11-08 19:41:56 +0900

    chore(#16):  package 기본 설정

---
+++ packages/react-markdown/vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  base: './',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setupTests.ts']
+  }
+});
