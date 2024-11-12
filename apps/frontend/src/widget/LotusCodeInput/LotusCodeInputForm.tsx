8afe24808b5845172301a53de7591e1fde483e40
ATeals <ateals@icloud.com>
2024-11-12 19:27:23 +0900

    feat(#34):  LotusCodeInput dnd
    framer-motion을 사용한 dnd 컴포넌트

--- apps/frontend/src/widget/LotusCodeInput/LotusCodeInputForm.tsx
+++ apps/frontend/src/widget/LotusCodeInput/LotusCodeInputForm.tsx
@@ -1,13 +1,14 @@
 import { Button, Input, Text } from '@froxy/design/components';
 import { cn } from '@froxy/design/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Reorder } from 'framer-motion';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-const lotusCodeInputValue = z.object({
+export const lotusCodeInputValue = z.object({
   items: z.array(
     z.object({
-      input: z.string().min(1, 'field is required')
+      input: z.string().min(1, '입력값은 필수 항목입니다.')
     })
   )
 });
@@ -31,26 +32,42 @@ export function LotusCodeInputForm(props: LotusCodeInputFormProps) {
     resolver: zodResolver(lotusCodeInputValue)
   });
 
-  const { fields, append, remove } = useFieldArray({
+  const { fields, append, remove, swap } = useFieldArray({
     control,
     name: 'items'
   });
 
   const onSubmit = (data: LotusCodeFormValues) => props.onSubmit?.({ data, reset });
 
+  const handleReorder = (newFields: typeof fields) => {
+    const firstDiffIndex = fields.findIndex((field, index) => field.id !== newFields[index].id);
+
+    if (firstDiffIndex === -1) return;
+
+    const newIndex = newFields.findIndex((field) => field.id === fields[firstDiffIndex].id);
+
+    swap(firstDiffIndex, newIndex);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={cn(props.className)}>
-      {fields.map((field, index) => (
-        <div key={field.id}>
-          <div className="flex gap-2">
-            <Input {...register(`items.${index}.input`)} placeholder={`Input ${index + 1}`} />
-            <Button onClick={() => remove(index)}>삭제</Button>
-          </div>
-          <Text variant="destructive" className="min-h-5 my-2">
-            {errors.items?.[index]?.input && errors.items[index].input?.message}
-          </Text>
-        </div>
-      ))}
+      <Reorder.Group drag="y" values={fields} onReorder={handleReorder}>
+        {fields.map((field, index) => (
+          <Reorder.Item key={field.id} value={field} className="hover:cursor-move">
+            <div className="flex gap-2 items-center">
+              <Text size="lg" className="w-10">
+                {index + 1}번
+              </Text>
+              <Input {...register(`items.${index}.input`)} placeholder={`Input ${index + 1}`} />
+              <Button onClick={() => remove(index)}>삭제</Button>
+            </div>
+
+            <Text variant="destructive" className="min-h-5 my-2 pl-12">
+              {errors.items?.[index]?.input && errors.items[index].input?.message}
+            </Text>
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
       <Button variant={'outline'} className="block w-full my-5" onClick={() => append({ input: '' })}>
         새로운 항목 추가
       </Button>
