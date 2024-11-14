d8e2de15a70f04356d7dcfdfce518f5f0c97ec5c
mjh000526 <mjh000526@gmail.com>
2024-11-15 01:01:59 +0900

    fix: env config path fix

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './config/typeorm.config';
@@ -11,12 +12,12 @@ import { GistModule } from './gist/gist.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: path.resolve(__dirname, '../../.env')
     }),
-    // TypeOrmModule.forRootAsync({
-    //   inject: [ConfigService],
-    //   useFactory: typeORMConfig
-    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: typeORMConfig
+    }),
     DockerModule,
     GistModule
   ],
