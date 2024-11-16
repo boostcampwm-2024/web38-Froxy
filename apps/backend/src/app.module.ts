5bb2557cb870c600a3bf3dae9014b9bc6efd99c2
kimhji <126762281+kimhji@users.noreply.github.com>
2024-11-16 15:21:01 +0900

    * feat(#53): github 로그인 url 보내기 및 기존 사용자가 아닐 경우 user 테이블에 추가
    * feat(#53): 로그인 시 토큰 만들어 보내기 및 유효 토큰 확인 구현
    
    * feat(#53): 리다이렉트 자동화
    
    * fix(#53): login 반환 값 json으로 확실히 변경
    
    * feat(#53): auth 모듈 전역화
    
    * feat(#53): api 전역 prefix 추가

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,10 +3,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { typeORMConfig } from './config/typeorm.config';
 import { DockerModule } from './docker/docker.module';
 import { GistModule } from './gist/gist.module';
+<<<<<<< HEAD
 import { HistoryModule } from './history/history.module';
+=======
+import { UserModule } from './user/user.module';
+>>>>>>> bad42e0 (#53 oauth api (#60))
 
 @Module({
   imports: [
@@ -20,7 +25,12 @@ import { HistoryModule } from './history/history.module';
     }),
     DockerModule,
     GistModule,
+<<<<<<< HEAD
     HistoryModule
+=======
+    UserModule,
+    AuthModule
+>>>>>>> bad42e0 (#53 oauth api (#60))
   ],
   controllers: [AppController],
   providers: [AppService]
