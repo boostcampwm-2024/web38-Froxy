3881d750582309d2223eb9d4df9445691629a4cd
ATeals <ateals@icloud.com>
2024-11-17 16:53:17 +0900

    feat(#73):  getLotusDetail, useLotusSuspenseQuery

--- apps/frontend/src/feature/Lotus/api.ts
+++ apps/frontend/src/feature/Lotus/api.ts
@@ -1,5 +1,7 @@
 import { LotusType } from './type';
 
+import { CodeViewValue } from '@/feature/CodeView';
+
 export const getLotusList = async ({ page = 1 }: { page: number }) => {
   const res = await fetch(`/api/lotus?page=${page}`);
 
@@ -9,4 +11,16 @@ export const getLotusList = async ({ page = 1 }: { page: number }) => {
     ...lotus,
     date: new Date(lotus.date)
   })) as LotusType[];
+};
+
+export const getLotusDetail = async ({
+  id
+}: {
+  id: string;
+}): Promise<LotusType & { language: string; files: CodeViewValue[] }> => {
+  const response = await fetch(`/api/lotus/${id}`);
+
+  const data = await response.json();
+
+  return { ...data, date: new Date(data.date) };
 };
