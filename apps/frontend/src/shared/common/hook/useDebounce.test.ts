87c4d6f4d17aec19854d9709ee6091cb8b2f650d
nakyong82 <nakyong7770@gmail.com>
2024-11-25 10:35:37 +0900

    refactor(#133): given-when-then 패턴 적용하기

--- apps/frontend/src/shared/common/hook/useDebounce.test.ts
+++ apps/frontend/src/shared/common/hook/useDebounce.test.ts
@@ -17,42 +17,56 @@ describe('useDebounce', () => {
   });
 
   test('delay값만큼 시간이 지나기 전에는 함수가 호출되지 않아야 한다.', () => {
+    //Given
     const { result } = renderHook(() => useDebounce(testFn, delay));
 
+    //When
     result.current();
     vitest.advanceTimersByTime(299);
+
+    //Then
     expect(testFn).not.toHaveBeenCalled();
   });
 
   test('delay값만큼 시간이 지났을 때 함수가 호출되어야 한다.', () => {
+    //Given
     const { result } = renderHook(() => useDebounce(testFn, delay));
 
+    //When
     result.current();
     vitest.advanceTimersByTime(300);
+
+    //Then
     expect(testFn).toHaveBeenCalledTimes(1);
   });
 
   test('연속 호출 시에 이전 타이머가 취소되어 이전 호출은 실행되지 않아야 한다.', () => {
+    //Given
     const { result } = renderHook(() => useDebounce(testFn, delay));
 
+    //When
     result.current();
     vitest.advanceTimersByTime(200);
 
     result.current();
     vitest.advanceTimersByTime(299);
 
+    //Then
     expect(testFn).not.toHaveBeenCalled();
   });
 
   test('연속 호출 시에 이전 타이머가 취소되고 마지막 호출만 실행되어야 한다.', () => {
+    //Given
     const { result } = renderHook(() => useDebounce(testFn, delay));
 
+    //When
     result.current();
     vitest.advanceTimersByTime(200);
 
     result.current();
     vitest.advanceTimersByTime(300);
 
+    //Then
     expect(testFn).toHaveBeenCalledTimes(1);
   });
 });
--- apps/frontend/src/shared/tagInput/useTagInput.test.tsx
+++ apps/frontend/src/shared/tagInput/useTagInput.test.tsx
@@ -20,33 +20,41 @@ describe('useTagInput', () => {
   });
 
   test('Enter 키 입력으로 태그가 추가되어야 한다.', () => {
+    //Given
     const { getByLabelText } = render(<TagInputComponent value={[]} onChange={mockOnChange} />);
-
     const input = getByLabelText('tag-input');
+
+    //When
     fireEvent.change(input, { target: { value: 'new tag' } });
     fireEvent.keyDown(input, { key: 'Enter' });
 
+    //Then
     expect(mockOnChange).toHaveBeenCalledWith(['new tag']);
     expect(input).toHaveValue('');
   });
 
   test('공백 문자열은 태그로 추가되지 않아야 한다.', () => {
+    //Given
     const { getByLabelText } = render(<TagInputComponent value={[]} onChange={mockOnChange} />);
-
     const input = getByLabelText('tag-input');
+
+    //When
     fireEvent.change(input, { target: { value: ' ' } });
     fireEvent.keyDown(input, { key: 'Enter' });
 
+    //Then
     expect(mockOnChange).not.toHaveBeenCalled();
   });
 
   test('removeTag 함수는 해당 태그를 삭제시켜야한다.', () => {
+    //Given
     const initialTags = ['tag1', 'tag2', 'tag3'];
-
     const { result } = renderHook(() => useTagInput(initialTags, mockOnChange));
 
+    //When
     result.current.removeTag(0);
 
+    //Then
     expect(mockOnChange).toHaveBeenCalledWith(['tag2', 'tag3']);
   });
 });
