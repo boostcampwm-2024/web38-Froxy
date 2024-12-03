f33f6c15397dba5bd4e250aa0edae6a107933bf0
mjh000526 <mjh000526@gmail.com>
2024-12-03 11:36:55 +0900

    build(#151): alwasy version test

--- apps/backend/src/docker/docker.module.ts
+++ apps/backend/src/docker/docker.module.ts
@@ -10,7 +10,7 @@ import { GistModule } from '@/gist/gist.module';
   imports: [
     GistModule,
     BullModule.registerQueue({
-      name: 'always-queue' // 큐 이름
+      name: 'always-queue'
     })
   ],
   controllers: [DockerController],
